b57baeda319267c5ad0048e6ee04ec5135b11e98
Mainak Jas <mainakjas@gmail.com>
2017-03-20 01:12:01 +0100

    Write groups to a file

--- explore_data.py
+++ explore_data.py
@@ -10,23 +10,41 @@ print('Total number of questions = %d' % unique_qids.shape[0])
 
 # ########### EXTRACT GROUPS ########
 
-groups = [[]]
-for idx in range(df.shape[0]):
-    print(idx)
-    if df['is_duplicate'][idx] == 1:
-        is_duplicate_pair = False
-        for g in groups:
-            if df['qid1'][idx] in g and df['qid2'][idx] not in g:
-                g.append(df['qid2'][idx])
-                is_duplicate_pair = True
-                break
-            elif df['qid2'][idx] in g and df['qid1'][idx] not in g:
-                g.append(df['qid1'][idx])
-                is_duplicate_pair = True
-                break
-        if not is_duplicate_pair:
-            groups.append([df['qid1'][idx], df['qid2'][idx]])
+qid1 = df['qid1'].values[df['is_duplicate'].values == 1]
+qid2 = df['qid2'].values[df['is_duplicate'].values == 1]
 
+groups = [[]]
+for idx in range(qid1.shape[0]):
+    if idx % 100 == 0:
+        print('%d / %d' % (idx, qid1.shape[0]))
+    is_duplicate_pair = False
+    for g in groups:
+        if qid1[idx] in g and qid2[idx] not in g:
+            g.append(qid2[idx])
+            is_duplicate_pair = True
+            break
+        elif qid2[idx] in g and qid1[idx] not in g:
+            g.append(qid1[idx])
+            is_duplicate_pair = True
+            break
+    if not is_duplicate_pair:
+        groups.append([qid1[idx], qid2[idx]])
+
+groups.remove([])
+
+lens = list()
 for g in groups:
-    if len(g) > 2:
-        print(g)
+    lens.append(len(g))
+idx = np.argsort(lens)[::-1]
+
+for i in idx[:20]:
+    print(groups[i])
+
+# ########### WRITE GROUPS ########
+
+lines = list()
+for i in idx:
+    lines.append(', '.join(str(e) for e in groups[i]))
+
+with open('data/groups.txt', 'w') as f:
+    f.write('\n'.join(lines))
